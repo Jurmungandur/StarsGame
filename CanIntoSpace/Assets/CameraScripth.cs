d658c2e157823f4c9929060cacac79e96e2e6289
Jurmungandur <31280587+Jurmungandur@users.noreply.github.com>
2018-10-31 22:03:59 +0100

    Alpha
    A funktional game

---
+++ CanIntoSpace/Assets/CameraScripth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScripth : MonoBehaviour {
+
+    public GameObject Player;
+
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y+2, Player.transform.position.z-10);
+	}
+}
