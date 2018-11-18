bf2156a1fa3214ac8da74232af5f302830b7ed25
Jurmungandur <31280587+Jurmungandur@users.noreply.github.com>
2018-11-18 16:27:32 +0100

    Beta v2

--- CanIntoSpace/Assets/Scripths/ControllerScripth.cs
+++ CanIntoSpace/Assets/Scripths/ControllerScripth.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class ControllerScripth : MonoBehaviour {
 
     public float targetSpeed = 0;
-    public float maxSpeed = 50;
+    public float maxSpeed = 30;
     public float currentSpeed;
     public float acc = 1f;
-    public float speed = 80;
+    public float speed = 60;
     public GameObject theMap;
+    Rigidbody rb;
 
     void Start () {
         currentSpeed = 0;
         //Debug.Log(currentSpeed);
+        rb = GetComponent<Rigidbody>();
+
     }
 	
 	void Update () {
@@ -53,26 +56,30 @@ public class ControllerScripth : MonoBehaviour {
         }
         //Debug.Log(currentSpeed);
 
-        //Camera Movment
-        Vector3 moveCamTo = transform.position - transform.forward * 10.0f + Vector3.up * 5.0f;
-        float bias = 0.96f;
-        Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1.0f-bias);
-        Camera.main.transform.LookAt(transform.position + transform.forward * 30.0f);
-
         //Movment
-        if (speed < 45)
+        if (speed < 35)
         {
-            speed = 45;
+            speed = 35;
         }
-
-        transform.position += transform.forward * Time.deltaTime * (speed + currentSpeed);
 
         speed -= transform.forward.y * Time.deltaTime * 25.0f;
 
         transform.Rotate(-Input.GetAxis("Vertical") * 1.2f, 0.0f, -Input.GetAxis("Horizontal") * 2f);
     }
 
+    void FixedUpdate() {
+        rb.velocity = transform.forward * (speed + currentSpeed);
+    }
+
     void OnCollisionEnter(Collision col) {
         Debug.Log("Hit");
+    }
+
+    void LateUpdate() {
+        //Camera Movment
+        Vector3 moveCamTo = transform.position - transform.forward * 10.0f + Vector3.up * 5.0f;
+        float bias = 0.96f;
+        Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1.0f - bias);
+        Camera.main.transform.LookAt(transform.position + transform.forward * 30.0f);
     }
 }
